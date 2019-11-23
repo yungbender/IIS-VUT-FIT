b915fa0a09983c506d7efccf4a06acd00d36ec7f
Tomáš Sasák <tsasak@redhat.com>
2019-11-23 16:09:04 +0100

    Fix showing comment form

--- templates/static/add_comment.js
+++ templates/static/add_comment.js
@@ -1,21 +1,18 @@
-function add_comment(parent)
-{
-if (parent == null)
-{
-   var parent = document.getElementById("new-comment-wrapper");
-}
-var childs = parent.childNodes;
-var childsCount = childs.lenght;
-for (var i = 0; i <= childsCount; i++)
-{
-   add_comment(childs[i])
-   if (childs[i].style.display === "none")
-   {
-      childs[i].style.display = "initial";
-   } else
-   {
-      child[i].style.display = "none";
-   }
-}
+var isShown = false;
 
+function add_comment()
+{
+   new_comment = document.getElementById("new-comment-wrapper");
+   if(isShown)
+   {
+      new_comment.classList.remove("new-comment-show");
+      new_comment.classList.toggle("new-comment-hide");
+      isShown = false;
+   }
+   else
+   {
+      new_comment.classList.remove("new-comment-hide");
+      new_comment.classList.toggle("new-comment-show");
+      isShown = true;
+   }
 }
