4fe513735b51eb35aab047e0306d5ffb601691d7
MartinKrajci <martin.krajci681@gmail.com>
2019-11-25 19:10:06 +0100

    Adding error pages, mainpage rework

--- mainpage.py
+++ mainpage.py
@@ -8,3 +8,23 @@ MAINPAGE_API = Blueprint("mainpage", __name__)
 def index():
     user = current_user
     return render_template("mainpage.html", user=user, search_image="/Static/search.png")
+
+@MAINPAGE_API.route("/error")
+def error():
+    user = current_user
+    return render_template("error.html", user=user)
+
+@MAINPAGE_API.route("/404")
+def e404():
+    user = current_user
+    return render_template("404.html", user=user)
+
+@MAINPAGE_API.route("/403")
+def e403():
+    user = current_user
+    return render_template("403.html", user=user)
+
+@MAINPAGE_API.route("/400")
+def e400():
+    user = current_user
+    return render_template("400.html", user=user)
