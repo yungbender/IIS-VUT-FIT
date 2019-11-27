be93b833d7b6d758887c5724d4762fd17aa0ce2e
yungbender <tomko251@gmail.com>
2019-11-27 22:56:36 +0100

    Fix gunicorn

--- webapp_run.sh
+++ webapp_run.sh
@@ -1,2 +1,4 @@
+touch ./logs/accesslog.log 
+chmod 777 ./logs/accesslog.log
 pipenv install
-pipenv run gunicorn --access-logfile accesslog.log --log-level info --workers=4 -b 0.0.0.0:6969 "app:create_app()"
+pipenv run gunicorn --access-logfile ./logs/accesslog.log --log-level info --workers=4 -b 0.0.0.0:6969 "app:create_app()"
